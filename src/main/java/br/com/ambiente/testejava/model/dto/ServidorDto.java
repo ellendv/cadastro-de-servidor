65ae3806088d3a668c632dc65ab94464ab3c3bf6
Ellen Veiga <ellendnveiga@gmail.com>
2022-05-19 14:29:03 -0300

    ?

--- src/main/java/br/com/ambiente/testejava/model/dto/ServidorDto.java
+++ src/main/java/br/com/ambiente/testejava/model/dto/ServidorDto.java
@@ -5,18 +5,21 @@ import lombok.Data;
 
 import java.util.Date;
 public class ServidorDto {
+    private Long id;
     private String nome;
     private String matricula;
     private Date dataCadastro;
     private LotacaoDto lotacaoDto;
 
 
-    public ServidorDto(String nome, String matricula, Date dataCadastro, LotacaoDto lotacaoDto) {
+    public ServidorDto(Long id, String nome, String matricula, Date dataCadastro, LotacaoDto lotacaoDto) {
+        this.id = id;
         this.nome = nome;
         this.matricula = matricula;
         this.dataCadastro = dataCadastro;
         this.lotacaoDto = lotacaoDto;
     }
+
 
     public String getNome() {
         return nome;
@@ -32,5 +35,10 @@ public class ServidorDto {
 
     public LotacaoDto getLotacaoDto() {
         return lotacaoDto;
+    }
+
+
+    public Long getId() {
+        return id;
     }
 }
--- src/main/java/br/com/ambiente/testejava/service/ServidorService.java
+++ src/main/java/br/com/ambiente/testejava/service/ServidorService.java
@@ -1,21 +1,26 @@
 package br.com.ambiente.testejava.service;
 
+import br.com.ambiente.testejava.model.Lotacao;
 import br.com.ambiente.testejava.model.Servidor;
 import br.com.ambiente.testejava.model.dto.ServidorDto;
+import br.com.ambiente.testejava.repositories.LotacaoRepository;
 import br.com.ambiente.testejava.repositories.ServidorRepository;
-import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ServidorService {
 
     private final ServidorRepository servidorRepository;
+    private final LotacaoRepository lotacaoRepository;
 
-    public ServidorService(ServidorRepository servidorRepository) {
+    public ServidorService(ServidorRepository servidorRepository, LotacaoRepository lotacaoRepository) {
         this.servidorRepository = servidorRepository;
+        this.lotacaoRepository = lotacaoRepository;
     }
 
     public Servidor cadastrarServidor(ServidorDto servidorDto){
+        Lotacao lotacao = new Lotacao();
+        lotacaoRepository.findById(servidorDto.getId()).orElseThrow(()-> new RuntimeException("lotacão não encontrada"));
         final var servidor = new Servidor(
                 servidorDto.getNome(),
                 servidorDto.getMatricula(),
